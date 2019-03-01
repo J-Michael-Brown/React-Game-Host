fbdb430c9ebf93c6602a2ccb70df517885e972d4
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-03-01 11:48:33 -0800

    add shape of game list and game info list

---
+++ src/assets/game-info-list.js
@@ -0,0 +1,15 @@
+const GameInfoList = {
+  'tic-tac-toe': {
+    name: 'Tic Tac Toe',
+    developerName: 'J. Michael Brown',
+    categories: [
+      'classic',
+      'strategy',
+      'local pvp',
+    ],
+    tags: [],
+    gameListId: 'tic-tac-toe',
+  }
+};
+
+export default GameInfoList
---
+++ src/assets/game-list.js
@@ -0,0 +1,8 @@
+import Game from '../games/Game';
+
+const GameList = {
+  'tic-tac-toe': {
+    component: Game,
+    id: 'tic-tac-toe',
+  }
+};
